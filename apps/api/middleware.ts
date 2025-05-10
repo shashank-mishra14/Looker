a2c5069bcd53777c9dd753a30b8f9445aa118e99
Shashank Mishra <shashank.mishra.dev14@gmail.com>
2025-05-10 20:12:53 +0530

    added middleware and api logic

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -5,16 +5,17 @@ import { JWT_PUBLIC_KEY } from "./config";
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers['authorization'];
     if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
     }
 
     const decoded = jwt.verify(token, JWT_PUBLIC_KEY);
     console.log(decoded);
     if (!decoded || !decoded.sub) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
     }
 
     req.userId = decoded.sub as string;
-    
-    next()
+    next();
 }
